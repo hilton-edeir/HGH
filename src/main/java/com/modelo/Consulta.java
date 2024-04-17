f1d90329b92056a6f583141ec7b2b194890c5441
Hilton Tavares <hilton.edeir@gmail.com>
2024-04-17 15:45:15 +0100

    update

--- src/main/java/com/modelo/Consulta.java
+++ src/main/java/com/modelo/Consulta.java
@@ -1,4 +1,68 @@
 package com.modelo;
 
 public class Consulta {
+    private int id;
+    private String data;
+    private String descricao;
+    private Paciente paciente;
+    private Tratamento tratamento;
+    private Receita_Medica receita_medica;
+    private Exame exame;
+
+    public Consulta(int id, String data, Paciente paciente, Tratamento tratamento, Receita_Medica receita_medica, Exame exame) {
+        this.id = id;
+        this.data = data;
+        this.paciente = paciente;
+        this.tratamento = tratamento;
+        this.receita_medica = receita_medica;
+        this.exame = exame;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData_consulta() {
+        return data;
+    }
+
+    public void setData_consulta(String data) {
+        this.data = data;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Tratamento getTratamento() {
+        return tratamento;
+    }
+
+    public void setTratamento(Tratamento tratamento) {
+        this.tratamento = tratamento;
+    }
+
+    public Receita_Medica getReceita_medica() {
+        return receita_medica;
+    }
+
+    public void setReceita_medica(Receita_Medica receita_medica) {
+        this.receita_medica = receita_medica;
+    }
+
+    public Exame getExame() {
+        return exame;
+    }
+
+    public void setExame(Exame exame) {
+        this.exame = exame;
+    }
 }
--- src/main/java/com/modelo/Exame.java
+++ src/main/java/com/modelo/Exame.java
@@ -1,4 +1,47 @@
 package com.modelo;
 
 public class Exame {
+    private String id;
+    private String data;
+    private String descricao;
+    private Tipo_Diagnostico tipoDiagnostico;
+
+    public Exame(String id, String data, String descricao, Tipo_Diagnostico tipoDiagnostico) {
+        this.id = id;
+        this.data = data;
+        this.descricao = descricao;
+        this.tipoDiagnostico = tipoDiagnostico;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Tipo_Diagnostico getTipoDiagnostico() {
+        return tipoDiagnostico;
+    }
+
+    public void setTipoDiagnostico(Tipo_Diagnostico tipoDiagnostico) {
+        this.tipoDiagnostico = tipoDiagnostico;
+    }
 }
