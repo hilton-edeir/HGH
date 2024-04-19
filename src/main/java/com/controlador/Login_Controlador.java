972541feec8af3e79c67f0a81adb6ec71b78307d
Hilton Tavares <hilton.edeir@gmail.com>
2024-04-19 20:58:44 +0100

    update

--- src/main/java/com/controlador/Login_Controlador.java
+++ src/main/java/com/controlador/Login_Controlador.java
@@ -3,7 +3,7 @@ package com.controlador;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Login_Controlador {
     @FXML
     private Label welcomeText;
 
